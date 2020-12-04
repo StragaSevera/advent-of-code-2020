e82c229da5e05505bdc17d109ae01abfcae1e681
StragaSevera <ought@yandex.ru>
2020-12-04 19:06:55 +0300

    Solve task 3-2

---
+++ src/main/java/day_03/FirstTask.kt
@@ -0,0 +1,26 @@
+package day_03
+
+import utils.resourceToString
+
+object FirstTask {
+    fun calculate(input: String): Int {
+        val data = prepareData(input)
+        return calculateResult(data)
+    }
+
+    private fun prepareData(input: String): List<String> = input.split("\n")
+
+    private const val TREE = '#'
+    private fun calculateResult(data: List<String>): Int = data.withIndex().count { (i, row) ->
+        val x = (i * 3) % row.length
+        row[x] == TREE
+    }
+}
+
+fun main() {
+    val input = resourceToString("day_03", "Task")
+    val result = FirstTask.calculate(input)
+    println()
+    println("======")
+    println("Result: $result")
+}
---
+++ src/test/java/day_03/FirstTaskTest.kt
@@ -0,0 +1,27 @@
+package ru.ought.advent_of_code.day_03
+
+import day_03.FirstTask
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class FirstTaskTest : FunSpec({
+    test("calculates correct answer") {
+        val input = """
+            ..##.......
+            #...#...#..
+            .#....#..#.
+            ..#.#...#.#
+            .#...##..#.
+            ..#.##.....
+            .#.#.#....#
+            .#........#
+            #.##...#...
+            #...##....#
+            .#..#...#.#
+        """.trimIndent()
+
+        val result = FirstTask.calculate(input)
+
+        result shouldBe 7
+    }
+})
